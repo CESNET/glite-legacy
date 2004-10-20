e7bb84312cb1320b268878dd3ff508e33be2e57c
David Collados <david.collados@cern.ch>
2004-10-20 10:19:58 +0000

    script to uninstall all the rpms

---
+++ org.glite.deployment.lb/config/scripts/remove_all_rpms.sh
@@ -0,0 +1,49 @@
+
+#!/bin/sh
+
+rpm -e edg-fetch-crl-1.0.0-EGEE \
+ca_ArmeSFo-0.23-1 \
+ca_ASGCCA-0.23-1 \
+ca_BEGrid-0.23-1 \
+ca_CERN-0.23-1 \
+ca_CESNET-0.23-1 \
+ca_CNRS-0.23-1 \
+ca_CNRS-DataGrid-0.23-1 \
+ca_CNRS-Projets-0.23-1 \
+ca_CyGrid-0.23-1 \
+ca_DOEGrids-0.23-1 \
+ca_DOESG-Root-0.23-1 \
+ca_ESnet-0.23-1 \
+ca_FNAL-0.23-1 \
+ca_FNAL_KCA-0.23-1 \
+ca_GermanGrid-0.23-1 \
+ca_Grid-Ireland-0.23-1 \
+ca_GridCanada-0.23-1 \
+ca_HellasGrid-0.23-1 \
+ca_INFN-0.23-1 \
+ca_IUCC-0.23-1 \
+ca_LIP-0.23-1 \
+ca_NIKHEF-0.23-1 \
+ca_NorduGrid-0.23-1 \
+ca_PK-Grid-0.23-1 \
+ca_PolishGrid-0.23-1 \
+ca_Russia-0.23-1 \
+ca_SlovakGrid-0.23-1 \
+ca_Spain-0.23-1 \
+ca_UKeScience-0.23-1 \
+glite-wms-utils-exception-0.1.0-0 \
+glite-wms-utils-jobid-0.1.0-0 \
+glite-lb-client-interface-0.2.0-0 \
+glite-lb-server-bones-0.0.0-0 \
+glite-lb-common-0.2.0-0 \
+glite-lb-logger-0.2.0-0 \
+glite-lb-server-0.3.0-0 \
+glite-security-proxyrenewal-0.1.0-1 \
+ares-1.1.1-EGEE \
+gpt-VDT1.2.0rh9-1 \
+vdt_globus_essentials-VDT1.2.0rh9-1 \
+perl-Expect.pm-1.01-9 \
+myproxy-1.14-EGEE \
+MySQL-client-4.0.20-0 \
+MySQL-server-4.0.20-0
+
