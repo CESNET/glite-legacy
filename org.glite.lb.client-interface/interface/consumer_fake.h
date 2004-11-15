5a63b500f0dbaf9a03872bd86c5fdbf84e3c5e12
František Dvořák <valtri@civ.zcu.cz>
2004-11-15 12:55:06 +0000

    producer API fake implementation and according examples polising of the fake consumer API code

--- org.glite.lb.client-interface/interface/consumer_fake.h
+++ org.glite.lb.client-interface/interface/consumer_fake.h
@@ -1,4 +1,6 @@
-/* $Id: */
+/* 
+ * fake implementation of the consumer API 
+ */
 
 #ifndef WORKLOAD_LOGGING_CLIENT_CONSUMER_FAKE_H
 #define WORKLOAD_LOGGING_CLIENT_CONSUMER_FAKE_H
@@ -8,9 +10,9 @@
 typedef int (edg_wll_QueryEvents_cb_f)(edg_wll_Context context, edg_wll_Event **events);
 typedef int (edg_wll_QueryListener_cb_f)(edg_wll_Context context, char **host, uint16_t *port);
 
-int egd_wll_RegisterTestQueryEvents(edg_wll_QueryEvents_cb_f *cb);
-int egd_wll_RegisterTestQueryListener(edg_wll_QueryListener_cb_f *cb);
-void egd_wll_UnregisterTestQueryEvents();
-void egd_wll_UnregisterTestQueryListener();
+int edg_wll_RegisterTestQueryEvents(edg_wll_QueryEvents_cb_f *cb);
+int edg_wll_RegisterTestQueryListener(edg_wll_QueryListener_cb_f *cb);
+void edg_wll_UnregisterTestQueryEvents();
+void edg_wll_UnregisterTestQueryListener();
 
 #endif /* WORKLOAD_LOGGING_CLIENT_CONSUMER_FAKE_H */
