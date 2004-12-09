13a319272fb7ebc842ba7e070f5268d60935c7d0
František Dvořák <valtri@civ.zcu.cz>
2004-12-09 14:04:06 +0000

    Replaced puzzling fake examples by unit test fake example. Updated documentation. Added C++isms to fake library header files.

--- org.glite.lb.client-interface/interface/consumer_fake.h
+++ org.glite.lb.client-interface/interface/consumer_fake.h
@@ -5,6 +5,10 @@
 #ifndef WORKLOAD_LOGGING_CLIENT_CONSUMER_FAKE_H
 #define WORKLOAD_LOGGING_CLIENT_CONSUMER_FAKE_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "glite/lb/context-int.h"
 
 typedef int (edg_wll_QueryEvents_cb_f)(edg_wll_Context context, edg_wll_Event **events);
@@ -14,5 +18,9 @@ int edg_wll_RegisterTestQueryEvents(edg_wll_QueryEvents_cb_f *cb);
 int edg_wll_RegisterTestQueryListener(edg_wll_QueryListener_cb_f *cb);
 void edg_wll_UnregisterTestQueryEvents();
 void edg_wll_UnregisterTestQueryListener();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* WORKLOAD_LOGGING_CLIENT_CONSUMER_FAKE_H */
