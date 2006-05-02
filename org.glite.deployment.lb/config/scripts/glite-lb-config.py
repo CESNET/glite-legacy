982019a6deebe872ad49bc0d4657126cc991b4e7
Joachim Flammer <joachim.flammer@cern.ch>
2006-05-02 10:36:19 +0000

    Changed deps on j2sdk to j2re.

--- org.glite.deployment.lb/config/scripts/glite-lb-config.py
+++ org.glite.deployment.lb/config/scripts/glite-lb-config.py
@@ -6,7 +6,7 @@
 # For license conditions see the license file or http://eu-egee.org/license.html
 #
 ################################################################################
-# glite-lb-config v. 2.2.1
+# glite-lb-config v. 2.2.3
 #
 # Post-installation script for configuring the gLite Logging and Bookkeping Server
 # Robert Harakaly < robert.harakaly@cern.ch >
@@ -48,7 +48,7 @@ class glite_lb:
     def __init__(self):
         self.mysql = MySQL.Mysql()
         self.verbose = 0
-        self.version = "2.2.1"
+        self.version = "2.2.3"
         self.name = "glite-lb"
         self.friendly_name = "gLite Logging and Bookkeeping"
         
