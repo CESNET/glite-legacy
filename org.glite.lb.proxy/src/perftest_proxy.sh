d28844da8b2633a1e81978c1629d17810d3d3f32
Michal Voců <michal@ruk.cuni.cz>
2006-05-31 15:48:29 +0000

    * correct producer arguments

--- org.glite.lb.proxy/src/perftest_proxy.sh
+++ org.glite.lb.proxy/src/perftest_proxy.sh
@@ -4,14 +4,14 @@ numjobs=$1
 
 # XXX - there must be better way to find stage
 STAGEDIR=/home/michal/shared/egee/jra1-head/stage
-. $STAGEDIR/bin/perftest_common.sh
+. $STAGEDIR/sbin/perftest_common.sh
 
 DEBUG=${DEBUG:-0}
 PERFTEST_CONSUMER=./glite_lb_proxy_perf 
 # CONSUMER_ARGS=
 # PERFTEST_COMPONENT=
 # COMPONENT_ARGS=
-LOGJOBS_ARGS="-x" 
+#LOGJOBS_ARGS="" 
 
 check_test_files || exit 1
 
@@ -29,6 +29,8 @@ do
 		j=$((j+1))
 	done
 	echo ""
+	# purge jobs from database
+	$LOGJOBS -n $numjobs > /tmp/perftest.jobids
 	i=$((i+1))
 done
 
