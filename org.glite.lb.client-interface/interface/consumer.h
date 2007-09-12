878c90cb4d9c97a8ee0e2b65c9414ffa31ccc3c0
František Dvořák <valtri@civ.zcu.cz>
2007-09-12 14:01:30 +0000

    New indexed column - lastUpdateTime.

--- org.glite.lb.client-interface/interface/consumer.h
+++ org.glite.lb.client-interface/interface/consumer.h
@@ -60,6 +60,7 @@ typedef enum _edg_wll_QueryAttr{
 	EDG_WLL_QUERY_ATTR_PARENT,	/**< Job was resubmitted */
 	EDG_WLL_QUERY_ATTR_EXITCODE,	/**< Unix exit code */
 	EDG_WLL_QUERY_ATTR_JDL_ATTR,	/**< Arbitrary JDL attribute */
+	EDG_WLL_QUERY_ATTR_LASTUPDATETIME,	/**< Time of the last known event of the job */
 	EDG_WLL_QUERY_ATTR__LAST
 /*	if adding new attribute, add conversion string to common/xml_conversions.c too !! */
 } edg_wll_QueryAttr;
